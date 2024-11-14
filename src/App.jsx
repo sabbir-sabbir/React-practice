625cf25109d1b155c096ea58153c41ae7010b0c5
sabbir-sabbir <iosabbir7890@gmail.com>
2024-11-14 22:44:20 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
 import { useState, useEffect } from 'react'
-import SingleUserData from './components/SingleUserData'
+
 import './App.css'
-import AnotherData from './components/AnotherData'
+
 
 
 function App() {
 
- const [myData, setmyData] = useState([])
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/albums")
-    .then(res => res.json())
-    .then(data => setmyData(data))
-  }, [])
-
-
+ 
   return (
     <>
-      <div className='grid grid-cols-3 gap-5 px-5 py-5'>
-         {
-          myData.map(singleData => <SingleUserData 
-          key ={singleData.id}
-          singleData = {singleData}
-          ></SingleUserData>)
-         }
-      </div>
-      <div>
-        <AnotherData></AnotherData>
-      </div>
+      
      
     </>
   )
